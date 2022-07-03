0e99775ba1721975422506ff73a30269f476673c
Nikola Nekudova <nikolka.nekudova@seznam.cz>
2022-07-03 20:19:55 +0200

    feat(): finished ui

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,40 +1,83 @@
 //test git
 const outcomes = ["rock", "paper", "scissors"];
+let scorePlayer = 0;
+let scoreComputer = 0;
 
 function computerPlay() {
     return outcomes[Math.floor((Math.random() * outcomes.length))];
 }
 
 // console.log(computerPlay(outcomes));
+let buttonArray = document.getElementsByClassName('btn');
+console.log(buttonArray)
 
-function round(playerSelection, computerSelection) {
-    playerSelection = playerSelection.toLowerCase();
-    computerSelection = computerSelection.toLowerCase();
+for (let i = 0; i < buttonArray.length; i++) {
+    buttonArray[i].addEventListener("click", function(clickEvent) {
+        let playerSelection = clickEvent.target.value;
+        let computerSelection = computerPlay();
+        console.log(computerSelection);
+        round(playerSelection, computerSelection);
+    })
+}
 
-    if (playerSelection === computerSelection) {
-        console.log("It is a draw!");
-        return null;
-    } else if (playerSelection === "rock" && computerSelection === "paper") {
-        console.log("You lose! Paper beats rock.");
-        return false;
-    } else if (playerSelection === "rock" && computerSelection === "scissors") {
-        console.log("You win! Rock beats scissors.");
-        return true;
-    } else if (playerSelection === "paper" && computerSelection === "rock") {
-        console.log("You win! Paper beats rock.");
-        return true;
-    } else if (playerSelection === "paper" && computerSelection === "scissors") {
-        console.log("You lose! Scissors beats paper.");
-        return false;
-    } else if (playerSelection === "scissors" && computerSelection === "rock") {
-        console.log("You lose! Rock beats scissors.");
-        return false;
-    } else if (playerSelection === "scissors" && computerSelection === "paper") {
-        console.log("You win! Scissors beats paper.");
-        return true;
+function displayResults (textResult, result) {
+    const resultDiv = document.createElement("div");
+    document.body.appendChild(resultDiv);
+    resultDiv.innerHTML = textResult;
+    updateScore(result);
+}
+
+function updateScore (result) {
+
+    if (result === true) {
+        scorePlayer = scorePlayer + 1;
+    } else if (result === false) {
+        scoreComputer = scoreComputer + 1;
+    }
+
+    const updatePlayer = document.getElementById("scorePlayer");
+    updatePlayer.innerHTML = scorePlayer;
+
+    const updateComputer = document.getElementById("scoreComputer");
+    updateComputer.innerHTML = scoreComputer;
+
+    checkAndDisplayWinner(scorePlayer, scoreComputer);
+}
+
+function checkAndDisplayWinner (scorePlayer, scoreComputer) {
+    if (scorePlayer == 5) {
+        alert("Player is winner!") 
+    } else if (scoreComputer == 5) {
+        alert("Computer is winner!") 
     }
 }
 
+function round(playerSelection, computerSelection) {
+    computerSelection = computerSelection.toLowerCase();
+
+    if (playerSelection === computerSelection) {
+        displayResults("It is a draw!", null);
+
+    } else if (playerSelection === "rock" && computerSelection === "paper") {
+        displayResults("You lose! Paper beats rock.", false);
+
+    } else if (playerSelection === "rock" && computerSelection === "scissors") {
+        displayResults("You win! Rock beats scissors.", true);
+
+    } else if (playerSelection === "paper" && computerSelection === "rock") {
+        displayResults("You win! Paper beats rock.", true)
+
+    } else if (playerSelection === "paper" && computerSelection === "scissors") {
+        displayResults("You lose! Scissors beats paper.", false)   
+
+    } else if (playerSelection === "scissors" && computerSelection === "rock") {
+        displayResults("You lose! Rock beats scissors.", false)   
+
+    } else if (playerSelection === "scissors" && computerSelection === "paper") {
+        displayResults("You win! Scissors beats paper.", true) 
+    }
+}
+/*
 let scorePlayer = 0;
 let scoreComputer = 0;
 
@@ -65,5 +108,5 @@ if (scorePlayer > scoreComputer) {
 } else { 
     alert("It is a draw.");
 }
-
+*/
 
